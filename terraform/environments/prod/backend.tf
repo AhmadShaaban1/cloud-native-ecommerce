c2de2d1ddc06b77c28cf6599014f56f971f143ce
ahmed_shaaban <ahmed@devops.com>
2026-02-01 21:49:57 +0200

    last commit

--- terraform/environments/prod/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "ecommerce-terraform-state-prod-ahmed"
-    key            = "prod/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "ecommerce-terraform-lock-prod"
-  }
-}
